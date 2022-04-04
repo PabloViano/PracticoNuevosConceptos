92cba9888ce5ac9291a12810bbbc3e77607b09de
Pablo_Viano <pabloviano1@gmail.com>
2022-04-03 21:24:19 -0300

    Punto 6-16

--- NuevosConceptos/NuevosConceptos/Extenciones.cs
+++ NuevosConceptos/NuevosConceptos/Extenciones.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace NuevosConceptos
 {
@@ -51,6 +52,77 @@ namespace NuevosConceptos
                 Resta -= Enteros[i];
             }
             return Resta;
+        }
+        public static DateTime RetornarFecha (this string Fecha)
+        {
+            DateTime FechaRetornar = DateTime.Now;
+            int valor = 0;
+            try
+            {
+                FechaRetornar = DateTime.Parse(Fecha);
+            }
+            catch (Exception)
+            {
+                valor = -1;
+            }
+            finally
+            {
+                if (valor == -1)
+                {
+                    FechaRetornar = DateTime.MinValue;
+                }
+            }
+            return FechaRetornar;
+        }
+        public static string InvertirString (this string palabra)
+        {
+            char[] invertida = palabra.ToCharArray();
+            invertida.Reverse();
+            return invertida.ToString();
+        }
+        public static string UnirArray(this string[] array,char separador)
+        {
+            string palabra = "";
+            foreach (string item in array)
+            {
+                palabra += $"{item}{separador}";
+            }
+            return palabra;
+        }
+        public static bool ValidarEmail (this string email)
+        {
+            string FormatoEmail = @"[0-9a-zA-Z]+[@]+[a-zA-Z]+[.]+[a-z]";
+            if (Regex.IsMatch(email, FormatoEmail))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool ValidarCuil(this string cuil)
+        {
+            string FormatoCuil = @"\d{2}-\d{8}-\d";
+            if (Regex.IsMatch(cuil, FormatoCuil))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static Cliente Restar(this Cliente cliente1, Cliente cliente2)
+        {
+            if (cliente1.Cuit == cliente1.Cuit)
+            {
+                cliente1.Saldo = cliente1.Saldo - cliente2.Saldo;
+                return cliente1;
+            }
+            if (cliente1.Saldo < cliente2.Saldo)
+            {
+                return cliente1;
+            }
+            return cliente2;
+        }
+        public static int Restar (this int numero1, int numero2)
+        {
+            return numero1 - numero2;
         }
     }
 }
